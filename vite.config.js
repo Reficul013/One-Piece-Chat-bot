d2004d55ac215c0750d9521a4f5ac526006754b3
unknown <reficul761@gmail.com>
2025-05-18 01:37:08 +0530

    added render domain

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   preview: {
+    port: 10000,
+    host: true,
+    allowedHosts: ['one-piece-chat-bot.onrender.com'], 
 })
